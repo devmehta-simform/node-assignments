aa4fcf006721e1a02ed596b9f7f238c9e9c20207
dev mehta <dev.mehta@simformsolutions.com>
2025-03-12 10:58:56 +0530

    feat: add multer and handle file upload request

--- file-operations-node/index.js
+++ file-operations-node/index.js
@@ -3,9 +3,41 @@ import fs from "fs";
 import path from "path";
 import multer from "multer";
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		const uploadDir = path.join(__dirname, "uploads");
+		if (!fs.existsSync(uploadDir)) {
+			fs.mkdirSync(uploadDir);
+		}
+		cb(null, uploadDir);
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
+
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.end("hello world");
+	if (req.method === "POST" && req.url === "/upload") {
+		const multerMiddleware = upload.single("file");
+
+		multerMiddleware(req, res, (err) => {
+			if (err) {
+				res.statusCode = 500;
+				res.end("File upload failed: " + err.message);
+				return;
+			}
+
+			res.writeHead(200, { "Content-Type": "text/plain" });
+			res.end("File uploaded successfully");
+		});
+	} else {
+		res.statusCode = 500;
+		res.end("internal server error");
+	}
 });
 
 server.listen(3000, () => {
