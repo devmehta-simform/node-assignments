b3567a4a7fcd3b3b85c672c9721c92c4bf5d1b0b
dev mehta <dev.mehta@simformsolutions.com>
2025-03-17 17:19:28 +0530

    feat: add getPhase2Output

--- practical-2/src/index.ts
+++ practical-2/src/index.ts
@@ -1,8 +1,11 @@
 import { default as data } from "./db/shopSchedule.json";
 import { getPhase1Output } from "./utils/getPhase1Output";
+import { getPhase2Output } from "utils/getPhase2Output";
 
 const now = new Date();
 
 const DaysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] as const;
 
 console.log("phase 1: ", getPhase1Output(now, DaysOfWeek, data));
+
+console.log("phase 2: ", getPhase2Output(now, DaysOfWeek, data));
---
+++ practical-2/src/utils/getPhase2Output.ts
@@ -0,0 +1,58 @@
+import { convertTimeToDate } from "./convertTimeToDate";
+import { convertTo24hrFormat } from "./convertTo24hrFormat";
+
+export function getPhase2Output(
+	now: Date,
+	DaysOfWeek: readonly string[],
+	ShopSchedule: readonly ShopSchedule[]
+) {
+	let ithDaysSchedule,
+		i = 0;
+
+	while (ithDaysSchedule === undefined && i < DaysOfWeek.length) {
+		ithDaysSchedule = ShopSchedule.find(
+			(s) => s.day === DaysOfWeek[(now.getDay() + i) % DaysOfWeek.length]
+		);
+		i++;
+	}
+
+	if (ithDaysSchedule) {
+		const ithDaysNow = new Date(now);
+		ithDaysNow.setDate(ithDaysNow.getDate() + (i - 1));
+
+		const [openHr, openMin] = convertTo24hrFormat(ithDaysSchedule.open);
+
+		const [closeHr, closeMin] = convertTo24hrFormat(ithDaysSchedule.close);
+
+		const openDateTime = new Date(
+			ithDaysNow.getFullYear(),
+			ithDaysNow.getMonth(),
+			ithDaysNow.getDate(),
+			parseInt(openHr),
+			parseInt(openMin)
+		);
+
+		const closeDateTime = new Date(
+			ithDaysNow.getFullYear(),
+			ithDaysNow.getMonth(),
+			ithDaysNow.getDate(),
+			parseInt(closeHr),
+			parseInt(closeMin)
+		);
+
+		if (
+			now.getTime() >= openDateTime.getTime() &&
+			now.getTime() <= closeDateTime.getTime()
+		) {
+			return `Open, The shop will be closed within ${Math.ceil(
+				(closeDateTime.getTime() - now.getTime()) / (1000 * 60 * 60)
+			)} Hrs`;
+		} else {
+			return `Closed. The shop will be open after ${Math.ceil(
+				(openDateTime.getTime() - now.getTime()) / (1000 * 60 * 60)
+			)} Hrs`;
+		}
+	} else {
+		return "Closed for now. No further information is available";
+	}
+}
