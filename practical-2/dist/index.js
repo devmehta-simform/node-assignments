3cc661dfc1f6730a8a48e382d95bd343260d451f
dev mehta <dev.mehta@simformsolutions.com>
2025-03-17 18:09:48 +0530

    build: generate build files

--- practical-2/dist/index.js
+++ practical-2/dist/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-	(this && this.__importDefault) ||
-	function (mod) {
-		return mod && mod.__esModule ? mod : { default: mod };
-	};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const shopSchedule_json_1 = __importDefault(require("./db/shopSchedule.json"));
 const getPhase1Output_1 = require("./utils/getPhase1Output");
@@ -11,27 +9,6 @@ const getPhase2Output_1 = require("./utils/getPhase2Output");
 const getPhase3Output_1 = require("./utils/getPhase3Output");
 const now = new Date();
 const DaysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-console.log(
-	"phase 1: ",
-	(0, getPhase1Output_1.getPhase1Output)(
-		now,
-		DaysOfWeek,
-		shopSchedule_json_1.default
-	)
-);
-console.log(
-	"phase 2: ",
-	(0, getPhase2Output_1.getPhase2Output)(
-		now,
-		DaysOfWeek,
-		shopSchedule_json_1.default
-	)
-);
-console.log(
-	"phase 3: ",
-	(0, getPhase3Output_1.getPhase3Output)(
-		now,
-		DaysOfWeek,
-		shopSchedule_json_1.default
-	)
-);
+console.log("phase 1: ", (0, getPhase1Output_1.getPhase1Output)(now, DaysOfWeek, shopSchedule_json_1.default));
+console.log("phase 2: ", (0, getPhase2Output_1.getPhase2Output)(now, DaysOfWeek, shopSchedule_json_1.default));
+console.log("phase 3: ", (0, getPhase3Output_1.getPhase3Output)(now, DaysOfWeek, shopSchedule_json_1.default));
